fdc04b6e2e1e9bc1978a5bda4fa1fd85562c0ee9
user <user@owners-MacBook-Air.local>
2025-11-17 12:11:11 +0100

    Fill main method, add Javadoc

--- e-filling-station-network/src/main/java/FillingStationNetwork/Standort.java
+++ e-filling-station-network/src/main/java/FillingStationNetwork/Standort.java
@@ -1,22 +1,28 @@
 package FillingStationNetwork;
 
+import jdk.jshell.spi.ExecutionControl;
+
 import java.util.ArrayList;
 
 /**
  * Standort mit mehreren Ladepunkten
  */
 public class Standort {
+    /** Unique ID of the Standort */
     private String StandortId;
+    /** Name of the Standort */
     private String Name;
+    /** Address of the Standort */
     private String Adresse;
+    /** Ladepunkte of this Standort */
     private ArrayList<Ladepunkt> Ladepunkte;
 
     /**
      * Konstruktor
-     * @param StandortId
-     * @param Name
-     * @param Adresse
-     * @param Ladepunkte
+     * @param StandortId Unique ID of the Standort
+     * @param Name Name of the Standort
+     * @param Adresse Address of the Standort
+     * @param Ladepunkte Ladepunkte of this Standort
      */
     public Standort(String StandortId, String Name, String Adresse,  ArrayList<Ladepunkt> Ladepunkte) {
         this.StandortId = StandortId;
@@ -28,9 +34,10 @@ public class Standort {
     /**
      * Aktiven Tarif abrufen
      * @return Aktiven Tarif
+     * @throws ExecutionControl.NotImplementedException Not implemented yet
      */
-    Object GetActiveTarifVersion() {
-        return null;
+    Object GetActiveTarifVersion() throws ExecutionControl.NotImplementedException {
+        throw new ExecutionControl.NotImplementedException("Not implemented yet");
     }
 
     /**
